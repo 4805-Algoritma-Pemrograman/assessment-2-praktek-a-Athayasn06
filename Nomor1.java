ada01b6348d9fe754aaf4e7278ca1e570bcb20c7
Athayasn06 <athayasn06@gmail.com>
2024-12-12 09:21:58 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,6 +6,22 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+        public static void bilanganGanjil(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan bilangan n: ");
+        int n = scanner.nextInt();
+        
+        bilanganGanjil(n);
+         scanner.close();
+        
 
         /* Pekerjaan anda berakhir sini */
 
